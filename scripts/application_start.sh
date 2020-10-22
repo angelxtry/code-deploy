6ea70030b7cca337539d58d975669c1096545326
angelxtry <angelxtry@gmail.com>
2020-10-22 16:33:01 +0900

    retry

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,12 @@
 if [ -d "/home/ubuntu/deplpy-test" ]
 then
-  cd "/home/ubuntu/deplpy-test"
-  yarn stop
-  sudo rm -rf "/home/ubuntu/deplpy-test"
+  sudo chown ${USER:=$(/usr/bin/id -run)}:$USER "/home/ubuntu/deplpy-test";
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+cd "/home/ubuntu/deplpy-test"
+
+yarn && yarn prod
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -11,3 +11,9 @@
 # cd "/home/ubuntu/deplpy-test"
 
 # yarn
+if [ -d "/home/ubuntu/deplpy-test" ]
+then
+  cd "/home/ubuntu/deplpy-test"
+  yarn stop
+  sudo rm -rf "/home/ubuntu/deplpy-test"
+fi
