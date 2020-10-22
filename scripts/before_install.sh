20ed471251635a07320359897ec03e0cc2217f17
angelxtry <angelxtry@gmail.com>
2020-10-22 14:21:43 +0900

    retry

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,3 @@
-if [ -d "/home/ubuntu/deplpy-test" ]; then sudo chmod ubuntu:ubuntu "/home/ubuntu/deplpy-test"; fi
+if [ -d "/home/ubuntu/deplpy-test" ]; then sudo chown ubuntu:ubuntu "/home/ubuntu/deplpy-test"; fi
 cd "/home/ubuntu/deplpy-test"
 yarn
