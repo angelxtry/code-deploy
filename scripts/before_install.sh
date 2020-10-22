2dd5ce51c163ac96847b69bdb702ba3a54b074de
angelxtry <angelxtry@gmail.com>
2020-10-22 14:26:23 +0900

    retry

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,9 @@
 if [ -d "/home/ubuntu/deplpy-test" ]; then sudo chown ubuntu:ubuntu "/home/ubuntu/deplpy-test"; fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 cd "/home/ubuntu/deplpy-test"
+
 yarn
