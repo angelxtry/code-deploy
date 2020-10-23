e76867fe9b8cb899c69505c121f2c3bfdba79c82
angelxtry <angelxtry@gmail.com>
2020-10-23 11:00:32 +0900

    2차 서버 nvm load

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,9 +3,9 @@
 #   sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER "$HOME/deplpy-test";
 # fi
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 cd "$HOME/deplpy-test"
 
