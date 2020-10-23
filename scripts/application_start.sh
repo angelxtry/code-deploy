ddea42cc47f06324b3ea780b3bde4dc66cc9f7d8
angelxtry <angelxtry@gmail.com>
2020-10-23 10:55:41 +0900

    2차 서버 테스트 one more

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,11 +1,11 @@
-if [ -d "$HOME/deplpy-test" ]
-then
-  sudo chown ${USER:=$(/usr/bin/id -run)}:$USER "$HOME/deplpy-test";
-fi
+# if [ -d "$HOME/deplpy-test" ]
+# then
+#   sudo chown -R ${USER:=$(/usr/bin/id -run)}:$USER "$HOME/deplpy-test";
+# fi
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 cd "$HOME/deplpy-test"
 
